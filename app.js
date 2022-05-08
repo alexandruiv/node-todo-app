81c81f86470c35715faf6e0a3089a5da41b45793
Alexandru Ivan <alex.ivan5478@gmail.com>
2022-05-08 16:10:08 +0300

    Added Todo model

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var config = require('./config');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.use('/assets', express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+
+mongoose.connect(config.getDbConnectionString());
+
+app.listen(port);
